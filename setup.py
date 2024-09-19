dd1171594a72db399e6c26dc20f993e82de489c6
waelr1985 <waelr1985@gmail.com>
2024-09-19 16:49:29 +0300

    updated requirements.txt

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="multilingual assistant",
+    version="0.0.1",
+    author="waelr1985",
+    author_email="waelr1985@ineuron.ai",
+    packages=find_packages(),
+    install_requires=["SpeechRecognition","pipwin","pyaudio","gTTS","google-generativeai","python-dotenv","streamlit"]
+)
